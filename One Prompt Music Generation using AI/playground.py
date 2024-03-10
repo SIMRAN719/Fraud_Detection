9ea0a4feb7d8f79bf22c234d6e4823c6234a854c
Simran Sah <hasnarmis.work719@gmail.com>
2024-03-10 19:42:38 +0530

    gitignore

--- One Prompt Music Generation using AI/playground.py
+++ One Prompt Music Generation using AI/playground.py
@@ -1,12 +1,13 @@
 from requests import post
 from pydub import AudioSegment
 from io import BytesIO
-
+from dotenv import load_dotenv
+import os
 #Hugging face
 API="https://api-inference.huggingface.co/models/facebook/musicgen-small"
 #My API Key
-API_key="Bearer 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"
+API_Key=os.getenv('API_Key')
 song=input("Enter which type of music you want to listen :")
-a=post(API,headers={"Authorization":API_key},json={'inputs':song})
+a=post(API,headers={"Authorization":API_Key},json={'inputs':song})
 b=AudioSegment.from_file(BytesIO(a.content))
 b.export("new_music.mp3",format="mp3")
