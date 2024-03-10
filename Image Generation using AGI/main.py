159a4389277fd80c983d5da60cc2b0ae151ed4fa
Simran Sah <hasnarmis.work719@gmail.com>
2024-03-10 19:40:35 +0530

    gitignore

--- Image Generation using AGI/main.py
+++ Image Generation using AGI/main.py
@@ -2,25 +2,37 @@ import requests
 import io
 from PIL import Image
 import random
+from dotenv import load_dotenv
+import os
+
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-API_Key="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"
+apikey = os.getenv('API_Key')
 
 def query(payload):
-	response = requests.post(API_URL, headers={'Authorization':"Bearer {}".format(API_Key)}, json=payload)
-	return response.content
+    response = requests.post(API_URL, headers={'Authorization': "Bearer {}".format(apikey)}, json=payload)
+    return response.content
+
 def generate_image(prompt):
-    seed=random.randint(1,1000)
+    seed = random.randint(1, 1000)
     random.seed(seed)
-    payload={"inputs": prompt,'parameters':{'temperature':random.uniform(0.5,1.5)}}
-    '''
-    payload={"inputs": prompt,'parameters':{'temperature':random.uniform(0.5,1.5),
-            'top_p': random.uniform(0.8,1.0),
-            'num_beams': random.randint(1,5),
-            'diversity_penalty': random.uniform(0.5,1.5)}}'''
+    payload = {"inputs": prompt, 'parameters': {'temperature': random.uniform(0.5, 1.5)}}
     image_bytes = query(payload)
-    image = Image.open(io.BytesIO(image_bytes))
-    return image
-pmt=input("Enter you prompt :")
-image=generate_image(pmt)
-image.show()
-image.save("pic.png")
+    
+    print("Content received from API:", image_bytes)  # Print content received from API
+    
+    try:
+        # Attempt to open the image using PIL
+        image = Image.open(io.BytesIO(image_bytes))
+        return image
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+pmt = input("Enter your prompt: ")
+image = generate_image(pmt)
+
+if image is not None:
+    image.show()
+    image.save("pic.png")
+else:
+    print("Failed to generate the image.")
